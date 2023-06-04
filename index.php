b6f54176669ce690c3bac3480e285afc12373040
Hanifan Aditya <hanifanaditya34@gmail.com>
2023-06-04 11:41:48 +0700

    menambah paragraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>hello world!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam commodi facilis eveniet, rem culpa ad, quidem, quasi nesciunt eligendi perferendis vero ratione nemo porro impedit reiciendis dicta voluptas similique odio!</p>
 </body>
 </html>
